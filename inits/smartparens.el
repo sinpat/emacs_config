d77651cb8de0deb903d90af41a5bd33114619a7d
DarkWingMcQuack <np-hardass@protonmail.com>
2018-05-15 01:16:14 +0200

    deleted comments

--- inits/smartparens.el
+++ inits/smartparens.el
@@ -1,19 +1,6 @@
-;;; smartparens.el --- provides smartparens support  -*- lexical-binding: t; -*-
-
-;; Copyright (C) 2018
-
-;; Author:  <lukas@GentooPad>
-;; Keywords: extensions, languages, tools
-;;; Commentary: this file provides smartparens mode
-
-;;; Code:
 (use-package smartparens
   :ensure t
   :diminish smartparens-mode
   :config
   (smartparens-global-mode 1)
   (show-smartparens-global-mode 1))
-
-
-(provide 'smartparens)
-;;; smartparens.el ends here

7d890e88509a812bc6ce2f873357694f79867583
DarkWingMcQuack <np-hardass@protonmail.com>
2018-05-15 00:37:50 +0200

    deleted unused code

--- inits/cmake.el
+++ inits/cmake.el
@@ -2,12 +2,3 @@
   :mode (("/CMakeLists\\.txt\\'" . cmake-mode)
          ("\\.cmake\\'" . cmake-mode))
   :ensure t)
-
-
-
-(use-package auto-package-update
-  :ensure t
-  :config
-  (setq auto-package-update-delete-old-versions t)
-  (setq auto-package-update-hide-results t)
-  (auto-package-update-maybe))

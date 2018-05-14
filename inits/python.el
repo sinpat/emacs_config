d3a12e2cd8dde90e27e33b743d10c0d0ca77f809
DarkWingMcQuack <np-hardass@protonmail.com>
2018-05-15 01:14:50 +0200

    use :hook

--- inits/python.el
+++ inits/python.el
@@ -1,24 +1,17 @@
-;;;python.el
-
 (use-package python
   :ensure t
-  :commands python-mode
-  :init
-  (add-hook 'python-mode-hook 'flycheck-mode))
+  :commands python-mode)
 
 (use-package anaconda-mode
   :after python
   :ensure t
+  :hook
+  ((python-mode-hook . anaconda-mode)
+   (python-mode-hook . eldoc-mode))
   :init
-  (progn
-    (add-hook 'python-mode-hook 'anaconda-mode)
-    (add-hook 'python-mode-hook 'eldoc-mode))
   (setq anaconda-mode-eldoc-as-single-line t))
 
 (use-package company-anaconda
   :after anaconda-mode
   :ensure t
   :init (add-to-list 'company-backends 'company-anaconda))
-
-
-;;;python.el ends here

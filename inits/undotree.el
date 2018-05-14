5552fd6052a9feae65d42efb48a83866518039e3
DarkWingMcQuack <np-hardass@protonmail.com>
2018-05-15 01:18:19 +0200

    diminish undotree

--- inits/undotree.el
+++ inits/undotree.el
@@ -1,17 +1,10 @@
-;;;undotree.el
-
 (use-package undo-tree
   :ensure t
-  :defer t
+  :diminish
   :config
-
   (custom-set-variables
    '(undo-tree-history-directory-alist
 	 (quote (("." . "~/.emacs.d/undo/")))))
 
   (evil-leader/set-key
 	"u" 'undo-tree-visualize))
-
-
-
-;;;undotree.el ends here

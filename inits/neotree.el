8965f3c381cefaa48f0005d86929399c84668385
DarkWingMcQuack <np-hardass@protonmail.com>
2018-05-15 01:11:30 +0200

    use :commands

--- inits/neotree.el
+++ inits/neotree.el
@@ -1,17 +1,15 @@
-;;;neotree.el
-
 (use-package neotree
   :ensure t
+  :commands neotree-toggle
   :config
-
   (global-set-key [f8] 'neotree-toggle)
   (setq projectile-switch-project-action 'neotree-projectile-action)
   (add-hook 'neotree-mode-hook
             (lambda ()
               (define-key evil-normal-state-local-map (kbd "RET") 'neotree-enter)))
-  (use-package	all-the-icons
-	:ensure t)
   (setq neo-theme (if (display-graphic-p) 'icons 'arrow)))
 
-(provide 'neotree)
-;;; neotree.el ends here
+
+(use-package all-the-icons
+  :after neotree
+  :ensure t)

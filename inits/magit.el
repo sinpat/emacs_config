240033419517a1639bd65249978dbabfe57d8778
DarkWingMcQuack <np-hardass@protonmail.com>
2018-05-15 01:08:14 +0200

    delete comments

--- inits/magit.el
+++ inits/magit.el
@@ -1,19 +1,6 @@
-;;; magit.el --- provides the magit package          -*- lexical-binding: t; -*-
-
-;; Copyright (C) 2018
-
-;; Author: ;;;magit.el <lukas@GentooPad>
-;; Keywords: tools, languages
-
-;;; Commentary:
-;; this file adds the magit package to my Emacs config
-;;; Code:
-
-
 (use-package magit
   :ensure t
   :commands magit-status)
-
 
 (use-package evil-magit
   :ensure t
@@ -22,6 +9,3 @@
   (evil-leader/set-key
     "gs" 'magit-status
     "gp" 'magit-push))
-
-(provide 'magit)
-;;; magit.el ends here

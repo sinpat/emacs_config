3456da2c2634438e36a38ab8219f006d22ec2809
DarkWingMcQuack <np-hardass@protonmail.com>
2018-05-16 17:05:50 +0200

    prettified for loops in c++ with the elem symbol

--- inits/prettify-symbols.el
+++ inits/prettify-symbols.el
@@ -18,6 +18,34 @@
            ("NULL"  .   "∅")
            ("null"  .   "∅")))))
 
+;; pretiffy c++ range based for loops
+(defvar pretty-for-rgx
+  (rx
+   "for("
+   (optional
+    "const"
+    (1+ space))
+   (1+ word)
+   (? "&")
+   (? "&")
+   (1+ space)
+   (1+ word)
+   (1+ space)
+   (group ":")
+   (1+ space)
+   (1+ word)))
+
+(defun pretty-for-add ()
+  (setq font-lock-keywords nil)
+  (font-lock-add-keywords
+   nil
+   `((,pretty-for-rgx 0 (prog1 nil
+                          (compose-region
+                           (match-beginning 1) (match-end 1)
+                           ,(concat "\t" (list #x2208))))))))
+
+(add-hook 'c-mode-common-hook 'pretty-for-add)
+
 (add-hook
  'python-mode-hook
  (lambda ()

f798d166a8e7df2417a0433e0c0640f207457b42
DarkWingMcQuack <np-hardass@protonmail.com>
2018-09-22 15:42:44 +0200

    disable semantic highliting

--- inits/cquery.el
+++ inits/cquery.el
@@ -1,23 +1,24 @@
-(use-package cquery
-  :after lsp-mode
-  :commands lsp-cquery-enable
-  ;; :hook (c-mode-common . lsp-cquery-enable)
-  :config
-  (setq cquery-executable "cquery")
-  (setq cquery-extra-init-params '(:index (:comments 2)
-                                          :cacheFormat "msgpack"
-                                          :completion (:detailedLabel t))))
+;; (use-package cquery
+;;   :after lsp-mode
+;;   :commands lsp-cquery-enable
+;;   :hook (c-mode-common . lsp-cquery-enable)
+;;   :config
+;;   (setq cquery-executable "cquery")
+;;   (setq cquery-extra-init-params '(:index (:comments 2)
+;;                                           :cacheFormat "msgpack"
+;;                                           :completion (:detailedLabel t))))
 
 (use-package ccls
   :after lsp-mode
   :commands lsp-ccls-enable
   :hook (c-mode-common . lsp-ccls-enable)
   :config
-  (setq ccls-sem-highlight-method 'font-lock)
-  (ccls-use-default-rainbow-sem-highlight)
-  (setq ccls-extra-args '("--log-file=/tmp/cq.log"))
+  ;;disable semantic highlighting, because it's retarded
+  ;; (setq ccls-sem-highlight-method 'font-lock)
+  ;; (ccls-use-default-rainbow-sem-highlight)
+  (setq ccls-extra-args '("--log-file=/tmp/ccls.log"))
   (setq ccls-cache-dir "/tmp/.ccls_cached_index")
   (setq ccls-executable "~/ccls/Release/ccls")
-  (setq ccls-extra-init-params '(:index (:comments 2)
+  (setq ccls-extra-init-params '(:index (:comments 1)
                                         :cacheFormat "msgpack"
                                         :completion (:detailedLabel t))))

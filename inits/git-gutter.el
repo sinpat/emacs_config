befed8108e4495ef11ecf7f9fbffeed9dca40764
DarkWingMcQuack <np-hardass@protonmail.com>
2018-12-19 18:00:11 +0100

    use git gutter fringe

--- inits/git-gutter.el
+++ inits/git-gutter.el
@@ -1,13 +1,5 @@
-(use-package git-gutter
+(use-package git-gutter-fringe
   :diminish git-gutter-mode
-  :config (global-git-gutter-mode)
-  :init
-  (setq git-gutter:separator-sign " "
-        git-gutter:lighter " GG")
   :config
-  (set-face-background 'git-gutter:deleted "#990A1B")
-  (set-face-foreground 'git-gutter:deleted "#990A1B")
-  (set-face-background 'git-gutter:modified "#00736F")
-  (set-face-foreground 'git-gutter:modified "#00736F")
-  (set-face-background 'git-gutter:added "#546E00")
-  (set-face-foreground 'git-gutter:added "#546E00"))
+  (setq git-gutter-fr:side 'right-fringe)
+  (global-git-gutter-mode))

5c9f19c0442304629821284d467467b48ee2234f
DarkWingMcQuack <np-hardass@protonmail.com>
2018-12-19 17:46:45 +0100

    dont use anaconda mode we use lsp with pyls

--- inits/python.el
+++ inits/python.el
@@ -1,14 +1,18 @@
 (use-package python
   :commands python-mode)
 
-(use-package anaconda-mode
-  :after python
-  :hook
-  ((python-mode-hook . anaconda-mode)
-   (python-mode-hook . eldoc-mode))
-  :init
-  (setq anaconda-mode-eldoc-as-single-line t))
+;; (use-package anaconda-mode
+;;   :after python
+;;   :hook
+;;   ((python-mode-hook . anaconda-mode)
+;;    (python-mode-hook . eldoc-mode))
+;;   :init
+;;   (setq anaconda-mode-eldoc-as-single-line t))
 
-(use-package company-anaconda
-  :after anaconda-mode
-  :init (add-to-list 'company-backends 'company-anaconda))
+;; (use-package company-anaconda
+;;   :after anaconda-mode
+;;   :init (add-to-list 'company-backends 'company-anaconda))
+
+;; (use-package lsp-python
+;;   :after lsp-mode
+;;   :hook (python-mode . lsp-python-enable))

55f4312b8147e89444ffc1847bf7f6bd688155bd
DarkWingMcQuack <np-hardass@protonmail.com>
2018-12-28 01:49:52 +0100

    add shorcut for helm-show-kill-ring

--- inits/helm.el
+++ inits/helm.el
@@ -1,14 +1,14 @@
 (use-package helm
   :demand t
   :diminish helm-mode
-  :bind (("M-x" . helm-M-x)
-         ("C-x C-f" . helm-find-files)
-         ("C-x b" . helm-buffers-list))
-  :init
+  :bind
+  ("M-x" . helm-M-x)
+  ("M-p" . helm-show-kill-ring)
+
+  :config
   (setq helm-M-x-fuzzy-match t
         helm-buffers-fuzzy-matching t
         helm-display-header-line nil)
-  :config
   (helm-mode 1)
   (helm-autoresize-mode 1))
 
